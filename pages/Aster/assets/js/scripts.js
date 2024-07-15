dc67a2c38bc3a2359caf90c6d7272b4c935b72ea
francesca1018 <202311583@fit.edu.ph>
2024-07-15 09:29:43 +0800

    feat Aster page

---
+++ pages/Aster/assets/js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('loading-screen').style.pointerEvents = 'none';
+    }, 5000);
+});
